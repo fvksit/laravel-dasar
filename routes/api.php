e97cc38c7aed70860f53ec28446cebb386a799c7
fvksit <dafikerja1@gmail.com>
2024-12-16 10:44:49 +0700

    API: Resource

--- routes/api.php
+++ routes/api.php
@@ -19,12 +19,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/books', [BookController::class, 'index']);
+// Route::get('/books', [BookController::class, 'index']);
 
-Route::post('/books', [BookController::class, 'store']);
+// Route::post('/books', [BookController::class, 'store']);
 
-Route::get('/books/{book}', [BookController::class, 'show']);
+// Route::get('/books/{book}', [BookController::class, 'show']);
 
-Route::put('/books/{book}', [BookController::class, 'update']);
+// Route::put('/books/{book}', [BookController::class, 'update']);
 
-Route::delete('/books/{book}', [BookController::class, 'destroy']);
+// Route::delete('/books/{book}', [BookController::class, 'destroy']);
+
+
+Route::apiResource('book', BookController::class);
